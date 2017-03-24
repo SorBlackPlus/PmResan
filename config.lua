c7f4ece84461f7256938e15ed5b38e86cce9fdcf
SorBlack <sorblackplus@gmail.com>
2017-03-24 12:30:59 -0700

    @sorblack :)

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
-bot_token = "245451534:AAEgrx-zGY_wRWkR0qYHi2xjSNL-qEjMZFU"
+bot_token = "364076548:AAEVk3CHtjmTDC4bzNoKbjhIpvGHciYx3bo"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "6.0"
-sudo_name = "Engineer Shayan Ahmadi"
-sudo_id = 259300954
-admingp = -135847900
-sudo_num = "989351372038"
-sudo_user = "shayan_soft"
-sudo_ch = "UmbrellaTeam"
+sudo_name = "(⒦-✞)admin cafe fanavarybot (sorblack)"
+sudo_id = 93365812
+admingp = -1001093528030
+sudo_num = "9109505426"
+sudo_user = "SorBlack"
+sudo_ch = "PrimeTeam"
